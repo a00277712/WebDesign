448952e51237f51641544e5945aa49a90a5e9997
a00277712 <a00277712@student.ait.ie>
2020-02-24 19:53:19 +0000

    Doc content into page

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php $title = "Home" ?>
 <?php include 'layout/header.inc' ?>
-    
+<div class="col">
+    <div class="row">
+        <div class="col-6">
+        </div>
+        <div class="col-6">At SNM Training we provide professional, high impact training programmes across multiple disciplines in all sectors.
+            By working together, we can provide relevant, flexible and affordable training to suit individual business needs.
+            Our aim is to support your business and employees to become more productive, efficient and competitive through upskilling and development Irish businesses.
+        </div>
+        <div class="col">
+            <h4 class=" d-flex justify-content-center">Testimonials</h4>
+            <div class="row d-flex justify-content-around">
+                <div class="testimonial col">“Thank you for supporting our Management Development Programmes. The Training was both efficient & professional.”<br/>M. Smith Ace Medical</div>
+                <div class="testimonial col">“Thank you for supporting our Management Development Programmes. The Training was both efficient & professional.”<br/>M. Smith Ace Medical</div>
+                <div class="testimonial col">“Thank you for supporting our Management Development Programmes. The Training was both efficient & professional.”<br/> M. Smith Ace Medical</div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include 'layout/footer.inc' ?>
