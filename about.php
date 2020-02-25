e8012950588f8e487162c0a27eb63d53b8153743
a00277712 <61272244+a00277712@users.noreply.github.com>
2020-02-25 15:17:30 +0000

    Moving map to about

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
 <?php $title = "About Us" ?>
 <?php include 'layout/header.inc' ?>
-    
+   <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d304853.0163623814!2d-8.761337166639471!3d53.34926722277762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x485c496f19ccea6b%3A0x1f3a077e5c4fdffd!2sAthlone%20Institute%20of%20Technology!5e0!3m2!1sen!2sie!4v1582573507730!5m2!1sen!2sie" width="600" height="450" frameborder="0" allowfullscreen=""></iframe>
 <?php include 'layout/footer.inc' ?>
