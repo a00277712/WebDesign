0732d44ba7edc475b275173507a3a7f31353ad6a
a00277712 <a00277712@student.ait.ie>
2020-03-02 22:13:15 +0000

    styling on home screen

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php $title = "Home" ?>
 <?php include 'layout/header.inc' ?>
-<div class="col">
+<div class="col page-content">
     <div class="row">
         <div class="col-6">
+        <h1>Welcome to<br>SNM Training </h1>
         </div>
         <div class="col-6">At SNM Training we provide professional, high impact training programmes across multiple disciplines in all sectors.
             By working together, we can provide relevant, flexible and affordable training to suit individual business needs.
             Our aim is to support your business and employees to become more productive, efficient and competitive through upskilling and development Irish businesses.
         </div>
-        <div class="col">
+        <div class="col testimonials">
             <h4 class=" d-flex justify-content-center">Testimonials</h4>
             <div class="row d-flex justify-content-around">
                 <div class="testimonial col">“Thank you for supporting our Management Development Programmes. The Training was both efficient & professional.”<br/>M. Smith Ace Medical</div>
